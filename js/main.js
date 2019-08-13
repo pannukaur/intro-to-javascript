c3421bc525312b639293a491c01371805f3e2b08
pannukaur <ssingh9442@yahoo.com>
2019-08-12 21:01:51 -0700

    one day is done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-!      
+let question;
+ function myfunk(){
+    let question = prompt('tell me your name.');
+    console.log('your name is ' + question);
+
+} 
+// console.log('js class');
+// onsole.log('js class is intresting')
+// console.log('best class');
+// console.log('hellow')
+
+function grabElementAndSpitOutValue()
+{
+    // det element from index
+    const MAIN_DIV=document.getElementById('mainDiv')
+    // write out in html
+    MAIN_DIV.innerHTML = 'hey , I am here';
+    // excute
+    console.log('this hit.');
+
+}
