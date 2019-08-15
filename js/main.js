79186b399b5c32713f704da85739bfd3c9665656
pannukaur <ssingh9442@yahoo.com>
2019-08-14 19:53:36 -0700

    day two done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-!      
+// ar firstName='kanwal'
+// var age = 37;
+
+// const firsName ='Kanwal';
+// firstName = 'dimple';
+
+// old way
+// console.log("My name is " + firstName + "my ge is" + age);
+// better way 
+// console.log('My name is ${firstName} and my age is ${age}');
+// alert (`hello, i am ${firstName}`); 
+// console.log('firstName');
+// const birthday ='25/01/1982';
+// let fixDate = new Date() ;
+// console.log(fixDate);
+// let hello = new string(1);
+// alert('hello');
+// console.log(typeof(hello));
+// const firstName = "dimple";
+// console.log(firstName.length);
+// const topThreeMovies = ['kids', 'romantic','immotional'];
+// console.log(topThreeMovies);
+// console.log(`my second top movies is: ${topThreeMovies[1]}`);
