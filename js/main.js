9fae5c8ca4561810437cf41972a634d0bfc82854
pannukaur <ssingh9442@yahoo.com>
2019-08-21 09:13:32 -0700

    done day three

--- js/main.js
+++ js/main.js
@@ -21,17 +21,91 @@
 // const topThreeMovies = ['kids', 'romantic','immotional'];
 // console.log(topThreeMovies);
 // console.log(`my second top movies is: ${topThreeMovies[1]}`);
-const firstName =prompt('what is your first name');
-let lastName = prompt('what is your last name')
-console.log('my first name is ${firstName} ');
-alert (`hello ${lastName}`);
-let birthday = prompt('when is your birthday');
 
-confirm (`is this your birthday ${birthday}`);
-alert (birthday);
-let fixDate = new Date(birthday);
+// take home day two
+// const firstName =prompt('what is your first name');
+// let lastName = prompt('what is your last name')
+// console.log('my first name is ${firstName} ');
+// alert (`hello ${lastName}`);
+// let birthday = prompt('when is your birthday');
 
-alert(fixDate);
+// confirm (`is this your birthday ${birthday}`);
+// alert (birthday);
+// let fixDate = new Date(birthday);
 
+// alert(fixDate);
+
+// day three
+// const exampleString ='i am a string';
+// const exampleObjectString = new String('1');
+// const anotherString ='i am string';
+// const H1 = document.getElementById('greeting');
+
+// function checkExample() {
+// if(exampleString == anotherString){
+//  consol e.log('yes, ${exampleString} and ${anotherString} not equal');
+// }else{
+//     console.log('sorry, they are not equal at all');
+
+// }
+// }
+// checkExample();
+
+// const LuckyNumber = 20;
+// if (LuckyNumber <= 15 || LuckyNumber > 11){
+//     console.log('lucky number ${LuckyNumber}' );
+// }else{
+//     console.log('sorry, not lucky.');
+// }
+
+//done------  
+
+// let a = 5;
+// let b =2;
+// let result = a += b;
+// console.log(result);
+// done--------
+// 
+
+//  let a = '5.99';
+//  let transformMe = parseFloat(a, 2);
+//  console.log(`Before: ${a}, After: ${transformMe}`, typeof(a), typeof(transformMe));
+// done-------
+// 
+
+// let hello = 'Hello ';
+// let there ='there!';
+// let sentence = hello += there;
+// console.log(sentence);
+// done
+
+// let valueNan = 'i am needed nan';
+// let testNan = isNaN(valueNan);
+// testNan ? console.log('this is true') : console.log('this is false');
+
+// ---
+// if (testNan === true)
+// ---
+
+// swich statement
+// let askColor = prompt('pick up color : red, orange,blue. Await the following response....');
+// switch(askColor){
+//     case 'red':
+//         H1.innerHTML = `you chose ${askColor}.. which reminds me of `;
+//         break;
+//         case 'orange':
+//             H1:innerHTML =`you chose ${askColor}..default`;
+//           case  'blue' =`you chose ${askColor}..remind `;
+//           break;
+//           default:
+//               H1:innerHTML =`not follow the roule`;
+// }
+// ---
+let  age = 18;
+if (age < 18){
+    alert('they are not old enough');
+}else {
+    console.log('they are old enough')
+}
 
 
