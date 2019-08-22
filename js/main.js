fd0519d20f3c35311fcf7cfe27a5adab3538f442
pannukaur <ssingh9442@yahoo.com>
2019-08-21 20:26:55 -0700

    dayfour array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
-!      
+    // const greeting = "1" ;{
+    // if (greeting==="hi");
+    // console.log(`yes thise match`);
+    // }
+    // else if (greeting === "hellow");{
+    //     console.log(`yes,this match hello`);
+    // }
+    // else if (greeting === "hellow") ;{
+    //     console.log(`this match`);
+    // }
+    // else {
+    //     console.log(`none`);
+    // }
+
+    // -----Array---
+
+    // let myArrayofRandomeInfo = ['hawwa', 13, false, '2011' ];
+    // console.log(myArrayofRandomeInfo.length);
+    // console.log(myArrayofRandomeInfo[3]);
+    // console.log(Array.isArray(myArrayofRandomeInfo));
+    // myArrayofRandomeInfo.push('apple');
+    // console.log(myArrayofRandomeInfo.length);
+    // console.log(myArrayofRandomeInfo);
+    // myArrayofRandomeInfo.unshift('number one');
+    // console.log(myArrayofRandomeInfo[0]);
+    // myArrayofRandomeInfo.pop();
+    // console.log(myArrayofRandomeInfo);
+    // myArrayofRandomeInfo.shift();
+    // console.log(myArrayofRandomeInfo);
+    // myArrayofRandomeInfo.splice(1, 1,5);
+    // console.log(myArrayofRandomeInfo); 
+    // myArrayofRandomeInfo.splice(1, 0);
+    // console.log(myArrayofRandomeInfo);
+    // let newArrayofInfo =myArrayofRandomeInfo.slice(1);
+    // console.log(newArrayofInfo);
+    // let findPositionToRemove = newArrayofInfo.indexof('2011');
+    // console.log(newArrayofInfo);
+    // let refereshed = newArrayofInfo.reverse();
+    // console.log(refereshed);
+     let myFavorietMovie = ['hawa', 'jadoo', 'lucky','rosi', 'chori-chori','bhoot'];
+     alert('my favorite move is' );
+     let myLessFavoriteMovies = ['bhoot','rosi','hawa'];
+     console.log(myLessFavoriteMovies);
+     let finalArray = ['jadoo',]
+     alert(myLessFavoriteMovies.lenght);
+
+
+
+
+    
+    
