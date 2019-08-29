3a7cb80160c62902e5c675663067eba705f6513a
pannukaur <ssingh9442@yahoo.com>
2019-08-28 18:00:30 -0700

    done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-!      
+
+const check = prompt('value is right');
+function square(x12, y15) // Name is a parameter  
+{ 
+    return x12 * y15;
+    
+} 
+alert(' NaN');
+ console.log(square(12, 15));
