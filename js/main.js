bc7db833dc1ca5602198b250a74039491eeeede4
pannukaur <ssingh9442@yahoo.com>
2019-09-04 17:58:38 -0700

    takehome-day-seven

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-!      
+   
+  function clickPrompt() { 
+    var doc = prompt("Please answer some question", 
+); 
+    if (doc != null) { 
+        document.getElementById("clickPrompt").innerHTML = 
+            "Hello " + doc; 
+    } 
+} 
+
+document.write("<h2><em>Hello world, again!</em></h2>");
+document.write("<p>This document was last modified on "
+    + document.lastModified + ".</p>");
+
+  
